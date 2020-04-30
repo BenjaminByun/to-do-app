dcae9047741b11cb072263940e24157b8b0e4ad5
Benjamin Byun <benjaminbyun@gmail.com>
2020-04-30 18:31:32 -0400

    DOM-part-2 completed

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 
 function onReady() {
+    const addToDoForm = document.getElementById('addToDoForm');
+
   let toDos = [];
-  const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
-  function createNewToDo(){
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
-    ToDos.push({
-      title: newToDoText.value,
-      complete: false
-    }
-    addToDoForm.addEventListener('submit', event => {
-      event.preventDefault();
-      createNewToDo();
-    });
-  );
-    newToDoText.value = '';
-
-    renderTheUI();
-  }
-  function renderTheUI();{
-    const toDoList = documentByID('toDoList');
-
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
@@ -29,40 +14,67 @@ function onReady() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = "Delete!"
+
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-    });
-  }
+      newLi.appendChild(deleteBtn);
+    })
 }
+
+  function createNewToDo(){
+    const newToDoText = document.getElementById('newToDoText');
+    if(!newToDoText.value){ return; }
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id
+    });
+
+    id++;
+
+    console.log(toDos);
+
+    newToDoText.value = '';
+    renderTheUI();
+}
+
+  addToDoForm.addEventListener('submit', event => {
+      event.preventDefault();
+      createNewToDo();
+    });
+
+  renderTheUI();
+}
+
+  //
+  //   const toDoList = documentByID('toDoList');
+  //
+  //   toDoList.textContent = '';
+  //
+  //   toDos.forEach(function(toDo) {
+  //     const newLi = document.createElement('li');
+  //     const checkbox = document.createElement('input');
+  //     checkbox.type = "checkbox";
+  //
+  //     newLi.textContent = toDo.title;
+  //
+  //     toDoList.appendChild(newLi);
+  //     newLi.appendChild(checkbox);
+  //   });
+  // }
+
 
 window.onload = function() {
   onReady();
 }
-//   const addToDoForm = document.getElementById('addToDoForm');
-//   const newToDoText = document.getElementById('newToDoText');
-//   const toDoList = document.getElementById('toDoList');
-// }
-//
-// addToDoForm.addEventListener('submit', event => {
-//   event.preventDefault();
-//
-//   let title = newToDoText.value;
-//   let newLi = document.createElement('li');
-//   let checkbox = document.createElement('input');
-//   checkbox.type = "checkbox";
-//
-//   let deleteBtn = document.createElement('button');
-//   deleteBtn.textContent = "Delete"
-//
-//   deleteBtn.addEventListener('click', function(event){
-//     toDoList.removeChild(this.parentElement);
-//   })
-//
-//   newLi.textContent = title;
-//   newLi.appendChild(checkbox);
-//   newLi.appendChild(deleteBtn);
-//   toDoList.appendChild(newLi);
-//   newToDoText.value = '';
-// });
